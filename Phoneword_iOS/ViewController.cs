2096cd374d1ae52260678ac465f9e98fc513367b
Shane Wright <sawright@tva.gov>
2016-10-11 15:57:14 -0400

    Updated view controller and phone translator.

--- Phoneword_iOS/ViewController.cs
+++ Phoneword_iOS/ViewController.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-
 using UIKit;
+using Foundation;
 
 namespace Phoneword_iOS
 {
@@ -15,6 +15,47 @@ namespace Phoneword_iOS
 		{
 			base.ViewDidLoad();
 			// Perform any additional setup after loading the view, typically from a nib.
+			string translatedNumber = "";
+
+			TranslateButton.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				// Convert the phone number with text to a number
+				// using PhoneTranslator.cs
+				translatedNumber = PhoneTranslator.ToNumber(
+					PhoneNumberText.Text);
+
+				// Dismiss the keyboard if text field was tapped
+				PhoneNumberText.ResignFirstResponder();
+
+				if (translatedNumber == "")
+				{
+					CallButton.SetTitle("Call ", UIControlState.Normal);
+					CallButton.Enabled = false;
+				}
+				else {
+					CallButton.SetTitle("Call " + translatedNumber,
+						UIControlState.Normal);
+					CallButton.Enabled = true;
+				}
+			};
+
+			CallButton.TouchUpInside += (object sender, EventArgs e) =>
+			{
+				// Use URL handler with tel: prefix to invoke Apple's Phone app...
+				var url = new NSUrl("tel:" + translatedNumber);
+
+				// ...otherwise show an alert dialog
+				if (!UIApplication.SharedApplication.OpenUrl(url))
+				{
+					var alert = UIAlertController.Create("Not supported", "Scheme 'tel:' is not supported on this device", UIAlertControllerStyle.Alert);
+					alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
+					PresentViewController(alert, true, null);
+				}
+			};
+
+			// todo: http://stackoverflow.com/questions/2143956/gitignore-for-visual-studio-projects-and-solutions
+			// todo: https://developer.xamarin.com/guides/ios/getting_started/hello,_iOS/hello,iOS_quickstart/
+			// step 27
 		}
 
 		public override void DidReceiveMemoryWarning()
