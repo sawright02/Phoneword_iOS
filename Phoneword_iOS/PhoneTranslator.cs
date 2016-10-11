7bdc16d7b7056ed3479b39ffa68090c3e0537988
Shane Wright <sawright@tva.gov>
2016-10-11 10:54:03 -0400

    Pushing initial code

---
+++ Phoneword_iOS/PhoneTranslator.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System;
+
+namespace Phoneword_iOS
+{
+	public static class PhoneTranslator
+	{
+		public static string ToNumber(string raw)
+		{
+			if (string.IsNullOrWhiteSpace(raw)) {
+				
+			}
+		}
+	}
+}
